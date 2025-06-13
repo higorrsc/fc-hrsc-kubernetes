49509f4af83a7bc3ca0ff0dfd748b21fec3a0aa3
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-06-13 20:55:33 -0300

    feat: :sparkles: add configMap injecting files

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
@@ -10,6 +11,7 @@ import (
 // "Hello Full Cycle!".
 func main() {
 	http.HandleFunc("/", Hello)
+	http.HandleFunc("/config-map", ConfigMap)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -17,4 +19,13 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello, I'm %s and I'm %s years old!", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("myFamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+
+	fmt.Fprintf(w, "My Family: %s", string(data))
 }
