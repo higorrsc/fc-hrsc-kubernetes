f74a4afa1971a14822ec7970c0b2144b498f49d8
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-06-18 19:17:17 -0300

    feat: :sparkles: add healthz endpoint to app

--- server.go
+++ server.go
@@ -5,7 +5,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+var startedAt = time.Now()
 
 // Main sets up a server that will respond to any request with the string
 // "Hello Full Cycle!".
@@ -13,6 +16,7 @@ func main() {
 	http.HandleFunc("/", Hello)
 	http.HandleFunc("/config-map", ConfigMap)
 	http.HandleFunc("/secret", Secret)
+	http.HandleFunc("/healthz", Healthz)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -35,4 +39,15 @@ func Secret(w http.ResponseWriter, r *http.Request) {
 	user := os.Getenv("USER")
 	password := os.Getenv("PASSWORD")
 	fmt.Fprintf(w, "User: %s\nPassword: %s", user, password)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+	if duration.Seconds() > 25 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	}
 }
